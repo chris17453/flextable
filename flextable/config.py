8ace9cd07a05d94f030f41168fcc89616dd76be4
Chris W <nd@localhost.localdomain>
2018-12-02 22:37:35 -0500

    Bump Version

--- flextable/config.py
+++ flextable/config.py
@@ -14,7 +14,7 @@ class table_config:
         self.delimiter=None
         self.delimiters={'field':',',  'comment':'#'}
         self.length=None
-        self.start.line=0
+        self.line=0
         self.page=0
         self.starts_on=1
         self.header=True
