8fb05d93b526d58376b14a107dc53eb9d2466d18
Chris W <nd@localhost.localdomain>
2018-12-02 22:16:30 -0500

    Bump Version

--- flextable/__init__.py
+++ flextable/__init__.py
@@ -1,3 +1,3 @@
-from table import table
+from table import table,table_config
 
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='flextable',
-    version='1.0.8',
+    version='1.0.9',
     packages=['flextable',],
     include_package_data=True,
     url='https://github.com/chris17453/flex-table/',
