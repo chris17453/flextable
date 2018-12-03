bd26dce35973cbe1e040b670eb09fd1545e857a5
Chris W <nd@localhost.localdomain>
2018-12-02 22:19:07 -0500

    Bump Version

---
+++ flextable/config.py
@@ -0,0 +1,27 @@
+class table_config:
+    def __init__(self):
+        self.file=None
+        self.remove_quote=True
+        self.block_quote=None
+        self.column_count=0
+        self.hide_comments=False
+        self.hide_errors=False
+        self.hide_whitespace=False
+        self.starts_on_line=1
+        self.header_on_line=0
+        self.columns=None
+        self.delimiters={'field':',',  'comment':'#'}
+        self.length=None
+        self.starts_on=1
+        self.header=True
+        self.footer=True
+        self.header_every=-1
+        self.width='auto'
+        # TODO
+        # exapanding from fixed point
+        self.tab_width=4
+        # TODO
+        # comments align on tabstops
+        self.tab_stop=8
+        self.row_heigh=-1
+        self.column_width=-1
--- flextable/table.py
+++ flextable/table.py
@@ -7,35 +7,9 @@ import curses
 
 from style import style
 from colors import *
+from config import table_config
 
 
-class table_config:
-    def __init__(self):
-        self.file=None
-        self.remove_quote=True
-        self.block_quote=None
-        self.column_count=0
-        self.hide_comments=False
-        self.hide_errors=False
-        self.hide_whitespace=False
-        self.starts_on_line=1
-        self.header_on_line=0
-        self.columns=None
-        self.delimiters={'field':',',  'comment':'#'}
-        self.length=None
-        self.starts_on=1
-        self.header=True
-        self.footer=True
-        self.header_every=-1
-        self.width='auto'
-        # TODO
-        # exapanding from fixed point
-        self.tab_width=4
-        # TODO
-        # comments align on tabstops
-        self.tab_stop=8
-        self.row_heigh=-1
-        self.column_width=-1
 
 class table:
 
